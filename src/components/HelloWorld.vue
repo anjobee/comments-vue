c7240812f3aafb599a14c70c2694a7dffb16e116
Anjo Bihis <angelo.bihis@maroonstudios.com>
2022-07-25 18:05:04 +0800

    chore: refactor code

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -25,44 +25,34 @@
     data: () => ({
       offsetTop: 0,
       offsetRight: "-999em",
-      commentableAreaHeight: 0,
-      rect: '',
-      popupRect: '',
+      commentableAreaBounds: '',
+      popupRectangleBounds: '',
       commentableElements: []
     }),
     mounted () {
       const commentableArea = document.getElementById('commentableArea')
-      for (const child of commentableArea.childNodes) {
-        if (child.id === 'popup') {
-          this.commentableElements.push(child.childNodes[0])
-        }
-        this.commentableElements.push(child)
-      }
-      const range = new Range()
-      range.selectNode(commentableArea)
-      var rect = range.getBoundingClientRect()
-      this.commentableAreaHeight = rect.height
-      this.rect = rect
       const popup = document.getElementById('popup')
-      const popupRange = new Range()
-      popupRange.selectNode(popup)
-      this.popupRect = popupRange.getBoundingClientRect()
-      window.addEventListener("mouseover", this.listenToHover);
+
+      this.getCommentableElements(commentableArea)
+
+      this.commentableAreaBounds = this.getBounds(commentableArea)
+      this.popupRectangleBounds = this.getBounds(popup)
+
+      window.addEventListener("mouseover", this.listenToHover)
     },
     destroyed() {
-      window.removeEventListener("mouseover", this.listenToHover);
+      window.removeEventListener("mouseover", this.listenToHover)
     },
     methods: {
       listenToHover (e) {
         if (e.target.id === 'popup') return
+
         if (this.commentableElements.includes(e.target)) {
           setTimeout(() => {
             if (e.target.id !== 'popupChild') {
-            const newRange = new Range()
-            newRange.selectNode(e.target)
-            const divRect = newRange.getBoundingClientRect()
-            const popupOffset = this.popupRect.height * 0.25
-            this.offsetTop = `${divRect.top - this.rect.top - popupOffset}px`
+            const targetBounds = this.getBounds(e.target)
+            const popupOffset = this.popupRectangleBounds.height * 0.25
+            this.offsetTop = `${targetBounds.top - this.commentableAreaBounds.top - popupOffset}px`
             }
             this.offsetRight = `5%`
           }, 0)
@@ -70,6 +60,19 @@
         else {
           this.offsetRight = '-999em'
         }
+      },
+      getBounds (node) {
+        const range = new Range()
+        range.selectNode(node)
+        return range.getBoundingClientRect()
+      },
+      getCommentableElements (commentableArea) {
+      for (const child of commentableArea.childNodes) {
+        if (child.id === 'popup') {
+          this.commentableElements.push(child.childNodes[0])
+        }
+        this.commentableElements.push(child)
+      }
       }
     }
   }
